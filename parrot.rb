a1034865ed237adb6545cf446b1b3024665fc89e
Estefano Livi <estefanolivi@outlook.com>
2020-07-07 21:04:14 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -2,13 +2,3 @@ def parrot(string = "Squawk!")
   puts string
   string 
 end
-puts "line 6"
-parrot
-puts "line 7"
-parrot("estefano")
-
-
-
-#def greeting(name = "Ruby programmer")
- # puts "Hello, #{name}"
-#end
